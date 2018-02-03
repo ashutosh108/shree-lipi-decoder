0c1d5a6458c79b28a8eef7a6bcb75b9d72c8acf6
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2018-02-03 19:10:41 +0200

    add test for long pipe write/read to test parallel reads/writes

--- test1/runner-test.cpp
+++ test1/runner-test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "../runner/runner.h"
 #include <exception>
+#include <sstream>
 
 TEST(RunnerTest, CannotCreateEmpty) {
 	try {
@@ -43,6 +44,24 @@ TEST(RunnerTest, FindStrWorksStdinInput) {
 	std::string s = run("findstr \"qwe\"", "a\r\nqwe\r\nqwe\r\n");
 	rtrim(s);
 	ASSERT_EQ("qwe\r\nqwe", s);
+}
+
+std::string repeat(const std::string &s, int times) {
+	std::ostringstream os;
+	for (int i = 0; i < times; ++i) {
+		os << s;
+	}
+	return os.str();
+}
+
+TEST(RunnerTest, Cat512KB) {
+	const int total_size = 512 * 1024;
+	std::string in = repeat("\r", total_size/4);
+	in += repeat("\n", total_size/4);
+	in += repeat("\r\n", total_size/8);
+	in += repeat("\n\r", total_size/8);
+	std::string res = run("cat.exe", in);
+	ASSERT_EQ(res, in);
 }
 
 TEST(PipeTest, CanCreate) {
