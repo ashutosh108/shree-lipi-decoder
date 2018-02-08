2f79499459ad2b61fa32be0b076ba7e888014777
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2018-02-08 15:44:00 +0200

    decoder: run qpdf with decompression options to create ...-decompressed.pdf

--- decoder/decoder.cpp
+++ decoder/decoder.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <string>
+#include <regex>
+#include <direct.h>
 
 #include "../runner/runner.h"
 
-int main() {
-	//"cpdf --stream-data=uncompress J:\\YandexDisk\\Reference\\B\\Books\\Madhva\\source\\vayustuti_pagemaker_files\\VayuStuti.pdf -");
-	std::cout << Runner::run("cmd /c dir");
+using namespace std::string_literals;
+
+void decompress_pdf(const std::string filename)
+{
+	auto decompressed_filename = std::regex_replace(
+		filename,
+		std::regex(R"RE(^(.*)(\.pdf)$)RE", std::regex::icase),
+		"$1-decompressed$2");
+	std::cout << decompressed_filename << std::endl;
+	//"qpdf --stream-data=uncompress J:\\YandexDisk\\Reference\\B\\Books\\Madhva\\source\\vayustuti_pagemaker_files\\VayuStuti.pdf -");
+	std::string cmdline = "../Dependencies/qpdf/bin/qpdf.exe --stream-data=uncompress "s + filename + " " + decompressed_filename;
+	std::cout << Runner::run(cmdline);
+	//std::cout << Runner::run("cmd /c dir");
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		std::cout << "Usage: decoder filename.pdf" << std::endl;
+		exit(1);
+	}
+
+	decompress_pdf(argv[1]);
 }
